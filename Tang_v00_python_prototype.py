69c9951edffbbcf99e91196028ba51ad456a0e7b
Min <milo.monsivais3@gmail.com>
2026-09-25 06:14:53 -0600

    Update to variable bytesize

--- Tang_v00_python_prototype.py
+++ Tang_v00_python_prototype.py
@@ -7,48 +7,48 @@ def SystemStart():
     global LIMB
     LIMB = []
     global LIMB_Size
-    LIMB_Size = 256
+    LIMB_Size = toInt("1"*byteSize)
     for x in range(LIMB_Size):
-        LIMB.append("00000000")
+        LIMB.append("0"*byteSize)
         
     global running
     running = True
         
-def to8string(number: int) -> str:
-    #Converts an integer between 0 and 255 into an 8-bit binary string.
+def toString(number: int) -> str:
+    #Converts an integer between 0 and 255 into an 8-bit binary string.  
     if not (0 <= number <= 255):
         raise ValueError("Number must be between 0 and 255 for an 8-bit byte.")
         
-    return f"{number:08b}"
+    return f"{number:0{byteSize}b}"
     
-def toInt8(bit_string: str) -> int:
+def toInt(bit_string: str) -> int:
     #Turns an 8-bit binary string into an integer.
-    if len(bit_string) != 8:
-        raise ValueError("The input string must be exactly 8 bits long.")
+    if len(bit_string) != byteSize:
+        raise ValueError(f"The input string must be exactly {byteSize} bits long.")
         
     return int(bit_string, 2)
 
 def newData(index, length):
-    if(LIMB[index] != "00000000"):
+    if(LIMB[index] != "0"*byteSize):
         raise ValueError("LIMB space is occupied")
-    LIMB[index] = to8string(length + index + 2)
-    LIMB[index + 1] = "00000000"
-    LIMB[index + length + 2] = to8string(index)
+    LIMB[index] = toString(length + index + 2)
+    LIMB[index + 1] = "0"*byteSize
+    LIMB[index + length + 2] = toString(index)
 
 def insertItem(indexOfData, atPos, inject): #The index of the Array, what you are injecting, and where in the array you want it to go.
-    if len(inject) != 8:
-        raise ValueError("The input string must be exactly 8 bits long.")
-    if(toInt8(LIMB[toInt8(LIMB[indexOfData])]) != indexOfData):
+    if len(inject) != byteSize:
+        raise ValueError(f"The input string must be exactly {byteSize} bits long.")
+    if(toInt(LIMB[toInt(LIMB[indexOfData])]) != indexOfData):
         raise ValueError("Selected LIMB is not closed.")
-    if(toInt8(LIMB[indexOfData])<=atPos + indexOfData):
+    if(toInt(LIMB[indexOfData])<=atPos + indexOfData):
         raise ValueError("LIMB is not long enough to contain data at specified index.")
 
     LIMB[indexOfData + 2 + atPos] = inject
    
 def addItem(index, add): #Streamlined method for adding to a LIMB array.
-    insertItem(index, toInt8(LIMB[index+1]), add)
-    indexPointer = toInt8(LIMB[index+1]) + 1
-    LIMB[index+1] = to8string(indexPointer)
+    insertItem(index, toInt(LIMB[index+1]), add)
+    indexPointer = toInt(LIMB[index+1]) + 1
+    LIMB[index+1] = toString(indexPointer)
     
 def getItem(source: int, index: int) -> int:
     return LIMB[source + 2 + index]
@@ -79,32 +79,47 @@ def splitString(input, delim, outpIndex):
         addItem(outpIndex, input[lastPoint:len(input)])
         
 def CitrusReadComponent(byte):
-    if len(byte) != 8:
-        raise ValueError("The input string must be exactly 8 bits long.")
-    if(byte == "00000001"):
+    if len(byte) != byteSize:
+        raise ValueError(f"The input string must be exactly {byteSize} bits long.")
+    if(byte == toString(1)):
         print("Component read successfully")
         
-    if(byte == "00000010"):
-        print("The current interpreter is Citrus Read")
+    if(byte == toString(2)):
+        print("The current interpreter is Citrus")
+        
+def newLIMB(name, length):
+    LIMB_Avaiable += length + 3
+    addItem(14)
+    
         
 def loadProcess(code):
-    splitString(code, "," , processLIMBaddress)
     
+    splitString(code, "," , processLIMBaddress)
+
+# Initialize important variables for Processes
 processLIMBaddress = 1
+LIMB_Avaiable = 0
+byteSize = 16
 #Start of program
 SystemStart()
 
 #There are many good reasons not to start data at 0.
 newData(processLIMBaddress,10)
-newData(14,1)
+newData(14,10)
+newData(27,10)
 
-loadProcess("00000001,00000010")
+loadProcess(f"{toString(1)},{toString(2)}")
 
+i2 = 0
+for x in range(50):
+    print(f"{LIMB[x]} ({toInt(LIMB[x])})    -    [{i2}]")
+    i2 += 1
 
 instructionPointer = 0
-count = toInt8(LIMB[2])
+count = toInt(LIMB[2])
 
+"""
 while(instructionPointer <= count):
     CitrusReadComponent(getItem(1,instructionPointer))
-    instructionPointer += 1 
+    instructionPointer += 1 """
     
